328d22c507feb70dbf6b8fa624c7119214acda0a
Kiran Gibson <kirangibson@Kirans-MacBook-Air.local>
2026-04-16 21:16:27 -0700

    chore: ignore .build directory

--- .build/Build/Products/Release/Tab Out Extension.appex/Contents/Resources/background.js
+++
@@ -1,8 +0,0 @@
-/**
- * background.js — Service Worker (Safari)
- */
-
-const _api = typeof browser !== 'undefined' ? browser : chrome;
-
-_api.runtime.onInstalled.addListener(() => {});
-_api.runtime.onStartup.addListener(() => {});
